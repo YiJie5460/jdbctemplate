0c1939e435e6e3695657c00d045e91d9c10f1160
YiJie5460 <yijie5460@foxmail.com>
2018-04-26 13:49:18 +0800

    1

--- src/main/java/SpringDAOImpl.java
+++ src/main/java/SpringDAOImpl.java
@@ -15,32 +15,33 @@ public class SpringDAOImpl implements DAO {
             new String[] {"applicationContext.xml"});
     JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
 
+    //增
     public void add(Student student) {
         String sql = "INSERT INTO student (name) VALUES(?);";
         jdbcTemplate.update(sql, student.getName());
     }
 
-
+    //改
     public void update(Student student) {
         String sql = "UPDATE student SET name = ? WHERE id  = ?;";
         jdbcTemplate.update(sql, student.getName(), student.getId());
     }
 
-
+    //删
     public void delete(int id) {
 
         String sql = "DELETE FROM student WHERE id  = ?;";
         jdbcTemplate.update(sql, id);
     }
 
-
+    //查
     public Student get(int id) {
         String sql = "select* FROM student WHERE id  = ?;";
         Student student = jdbcTemplate.queryForObject(sql, new MyRowMapper(), id);
         return student;
     }
 
-
+//重载，内部调用，限定默认最大值，防止溢出。
     public List<Student> list() {
         return list(0, Short.MAX_VALUE);
     }
